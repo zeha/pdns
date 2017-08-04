b216ea7a1094c6e84c8b6b1741c55491774bd0a1
Seth Ornstein <sornstein@globalcyberalliance.org>
2017-08-04 17:38:40 -0400

    Named Caches
    Up to commit "cleaned up source with better map",
    dated Wed Nov 22 09:36:47 2017 -0500.
    
    Squashed. dnsdist-lua2.cc additions transplanted into
    dnsdist-lua-namedcache.cc.

--- pdns/dnsdist-lua.cc
+++ pdns/dnsdist-lua.cc
@@ -1580,6 +1580,8 @@ vector<std::function<void(void)>> setupLua(bool client, const std::string& confi
   setupLuaRules();
   setupLuaVars();
 
+  setupLuaNamedCache(client);
+
   std::ifstream ifs(config);
   if(!ifs)
     warnlog("Unable to read configuration from '%s'", config);
---
+++ zzz-gca-example/various-configs/dnsdist-named-cache-test.sh
@@ -0,0 +1,19 @@
+echo "dnsdist2-named-cache-test.sh - test dnsdist with named cache version B"
+echo ""
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SLASH="/"
+CFG_FILE="dnsdist-named-cache-test.conf"
+CONFIG_FILE=$DIR$SLASH$CFG_FILE
+echo "current directory: " $DIR
+echo ""
+echo "configuration file: " $CONFIG_FILE
+echo ""
+echo "cd ../pdns/dnsdistdist"
+echo ""
+cd ../pdns/dnsdistdist
+echo ""
+###echo "listen on port 5200 for requests"
+###echo ""
+###./dnsdist --config=$CONFIG_FILE --local=0.0.0.0:5200
+
+./dnsdist --config=$CONFIG_FILE
